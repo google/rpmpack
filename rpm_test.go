de8fa896aeaa4db0ad103c5900bb5487d5852bf6
Yaron de Leeuw <jarondl@google.com>
2020-08-06 11:13:46 +0300

    Handle tar filemodes that overllap with 0120000
    We had bad logic to deduce symlinks. This caused us to think
    files with modes such as 0100644 are symlinks. All this change
    does is properly compare the bits.
    
    Fixes #49

--- rpm_test.go
+++ rpm_test.go
@@ -31,3 +31,27 @@ func TestFileOwner(t *testing.T) {
 		t.Errorf("File owner shoud be %s but is %s", group, r.filegroups[0])
 	}
 }
+// https://github.com/google/rpmpack/issues/49
+func Test100644(t *testing.T) {
+	r, err := NewRPM(RPMMetaData{})
+	if err != nil {
+		t.Errorf("NewRpm returned error %v", err)
+		t.FailNow()
+	}
+	r.AddFile(RPMFile{
+		Name:  "/usr/local/hello",
+		Body:  []byte("content of the file"),
+		Mode: 0o0100644,
+	})
+
+	if err := r.Write(ioutil.Discard); err != nil {
+		t.Errorf("Write returned error %v", err)
+	}
+	if r.filemodes[0] != 0100644 {
+		t.Errorf("file mode want 0100644, got %o", r.filemodes[0])
+	}
+	if r.filelinktos[0] != "" {
+		t.Errorf("linktos want empty (not a symlink), got %q", r.filelinktos[0])
+	}
+
+}
